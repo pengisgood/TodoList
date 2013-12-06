47581a04193b8f060764f16351977649416a3a42
Hongwei <hwpeng@Hongwei.local>
2013-12-06 14:48:17 +0800

    change the collection name

--- src/main/webapp/WEB-INF/js/service/mongolabService.js
+++ src/main/webapp/WEB-INF/js/service/mongolabService.js
@@ -4,5 +4,5 @@ var myApp = angular.module("myApp", ['mongolabResourceHttp']);
 myApp.constant('MONGOLAB_CONFIG', {API_KEY:'7lOuCyzkW57WlJLk9Zsu0VFCNpDi_1-J', DB_NAME:'todolistdb'});
 
 myApp.factory('mongolabService', function($mongolabResourceHttp) {
-    return $mongolabResourceHttp('mongolabService');
+    return $mongolabResourceHttp('todos'); //parameter is collection name
 });

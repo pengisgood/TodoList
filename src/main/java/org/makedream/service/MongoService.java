3533a61c71a17840e0bf2537791458f8de5c4070
Hongwei <hwpeng@Hongwei.local>
2013-12-22 20:34:03 +0800

    upgrade spring version to 4.0 to use rest controller

---
+++ src/main/java/org/makedream/service/MongoService.java
@@ -0,0 +1,9 @@
+package org.makedream.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface MongoService {
+
+
+}
---
+++ src/main/java/org/makedream/service/impl/MongoServiceImpl.java
@@ -0,0 +1,9 @@
+package org.makedream.service.impl;
+
+import org.makedream.service.MongoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MongoServiceImpl implements MongoService {
+
+}
